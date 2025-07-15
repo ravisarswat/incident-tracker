d1e56c63d3f5fb658afbcb965d76287e96f74220
Ravi sarswat <rmanan@Rmanans-MacBook-Pro.local>
2025-07-15 14:14:35 +0530

    Use Railway dynamic port

--- app.py
+++ app.py
@@ -175,5 +175,7 @@ def export_excel():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
